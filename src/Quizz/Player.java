9d5c4b7b94b1ee34fdccbbecab5116d96ec3876c
Vitoria <euvitoriamedeiros15@gmail.com>
2024-04-14 15:20:24 -0300

    feat: Class Player

---
+++ src/Quizz/Player.java
@@ -0,0 +1,19 @@
+package Quizz;
+
+public class Player extends Person {
+    private int score;
+
+    public Player(String name, int score) {
+        super(name);
+        this.score = score;   
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+   
+}
